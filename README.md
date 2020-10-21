910b119f52242621fd2b40ef190cacc7c4ea2661
Andras Bacsai <andras.bacsai@gmail.com>
2020-10-22 00:37:14 +0200

    :taco:

---
+++ README.md
@@ -0,0 +1,2 @@
+# Post about it
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {},
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*.svelte', './src/**/*.html'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
